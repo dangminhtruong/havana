38c92a5be968ff1ff92c0ef54eae345311740110
Dang Minh Truong <xdangminhtruongx@gmail.com>
2018-01-16 22:16:32 +0700

    Havana

--- public/admin/js/scripts.js
+++ public/admin/js/scripts.js
@@ -1,21 +1,13 @@
-(function() {
-    "use strict";
+$(function () {
+    $('#supported').text('Supported/allowed: ' + !!screenfull.enabled);
 
-    // custom scrollbar
-
-    $("html").niceScroll({styler:"fb",cursorcolor:"#1ABC9C", cursorwidth: '6', cursorborderradius: '10px', background: '#424f63', spacebarenabled:false, cursorborder: '0',  zindex: '1000'});
-
-    $(".scrollbar1").niceScroll({styler:"fb",cursorcolor:"rgba(97, 100, 193, 0.78)", cursorwidth: '6', cursorborderradius: '0',autohidemode: 'false', background: '#F1F1F1', spacebarenabled:false, cursorborder: '0'});
-
-	
-	
-    $(".scrollbar1").getNiceScroll();
-    if ($('body').hasClass('scrollbar1-collapsed')) {
-        $(".scrollbar1").getNiceScroll().hide();
+    if (!screenfull.enabled) {
+        return false;
     }
-
-})(jQuery);
-
+    $('#toggle').click(function () {
+        screenfull.toggle($('#container')[0]);
+    });
+});
                      
      
   
