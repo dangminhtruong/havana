ee2d90f704b65c0c535235f27bb194c2e7b7cfe6
Dang Minh Truong <xdangminhtruongx@gmail.com>
2018-01-21 12:58:31 +0700

    Havana

--- public/admin/js/scripts.js
+++ public/admin/js/scripts.js
@@ -9,5 +9,55 @@ $(function () {
     });
 });
                      
-     
-  
+var category_add = new Vue({
+    el : '#category_add',
+    data : {
+        categoryType : null,
+        categoryName : null,
+        categoryDesc : null,
+        alertTypeNull : null,
+        alertDescNull : null,
+        alertNameNull : null
+    },
+    methods : {
+        resetForm : function(){
+            this.categoryType = null;
+            this.categoryName = null;
+            this.categoryDesc = null;
+            this.alertTypeNull = null;
+            this.alertDescNull = null;
+            this.alertNameNull = null;
+        },
+        submited : function(){
+            if(this.categoryDesc == null){
+                return this.alertDescNull = 'This field is required';
+            }
+            else if(this.categoryName ==  null){
+                return this.alertNameNull = 'This field is required';
+            }
+            else if(this.categoryType == null){
+                return this.alertTypeNull = 'This field is required';
+            }else {
+                axios.post('/admin//category/add', {
+                    type : this.categoryType,
+                    name: this.categoryName,
+                    desc: this.categoryDesc,
+                })
+                .then(function (response) {
+                    if(response.data.status === 'inserted'){
+                        category_add.resetForm();
+                        toastr.options.closeButton = true;
+                        toastr.success('New category inserted!')
+                    }
+                })
+                .catch(function (error) {
+                    toastr.options.closeButton = true;
+                    toastr.warning('Opps!, something went wrong')
+                });
+            }
+        } 
+    },
+    computed : {
+
+    }
+});
--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const async = require('async');
 const Category = require('../model/category');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 /*------------------------------------
 * Author : Dang Minh Truong
@@ -15,13 +17,23 @@ router.get('/', function(req, res, next) {
 router.get('/category/add', (req, res, next) => {
     Category.find({}, {_id : 1, name : 1 })
     .exec((err, categories) => {
-        res.render('./admin/add_category', {
+        res.render('./admin/pages/add_category', {
             categories : categories
         });
     });
 });
 /*--------------------------------------------------------*/
-router.post('/category/add', (req, res, next) => {
+router.post('/category/add', urlencodedParser , (req, res, next) => {
+    let cate = new Category({
+        name : req.body.name,
+        type : req.body.type,
+        descript : req.body.desc
+    })
+    cate.save(function (err, results) {
+        res.send({
+            status : 'inserted'
+         });
+    });
     
 });
 
