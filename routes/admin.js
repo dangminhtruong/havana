a8107adc419861ab18b86828ba6f7d716ce20985
Dang Minh Truong <xdangminhtruongx@gmail.com>
2018-01-21 10:33:30 +0700

    Havana

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const async = require('async');
+const Category = require('../model/category');
 
 /*------------------------------------
 * Author : Dang Minh Truong
@@ -11,4 +12,17 @@ router.get('/', function(req, res, next) {
     res.render('./admin/index');
 });
 /*--------------------------------------------------------*/
+router.get('/category/add', (req, res, next) => {
+    Category.find({}, {_id : 1, name : 1 })
+    .exec((err, categories) => {
+        res.render('./admin/add_category', {
+            categories : categories
+        });
+    });
+});
+/*--------------------------------------------------------*/
+router.post('/category/add', (req, res, next) => {
+    
+});
+
 module.exports = router;
