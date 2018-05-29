4bcadc6f21b271b28507a85534f72df233187a69
dangminhtruong <xdangminhtruongx@gmail.com>
2018-05-29 18:14:04 +0700

    some update

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.use(function(err, req, res, next) {
 
 	// render the error page
 	res.status(err.status || 500);
-	res.render('error');
+	res.redirect('/');
 });
 //-------------------------------------------------------
 passport.serializeUser(function(user, done) {
